436e03825a2a4ed690bcca34d62e4a673212f699
your_name <your_address_email@example.com>
2023-01-26 09:55:56 +0100

    first commit

---
+++ MatrizBotones/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MatrizBotones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFila = new System.Windows.Forms.TextBox();
+            this.txtColumna = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // txtFila
+            // 
+            this.txtFila.Location = new System.Drawing.Point(42, 58);
+            this.txtFila.Name = "txtFila";
+            this.txtFila.Size = new System.Drawing.Size(100, 20);
+            this.txtFila.TabIndex = 0;
+            this.txtFila.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFila_KeyUp);
+            // 
+            // txtColumna
+            // 
+            this.txtColumna.Location = new System.Drawing.Point(191, 58);
+            this.txtColumna.Name = "txtColumna";
+            this.txtColumna.Size = new System.Drawing.Size(100, 20);
+            this.txtColumna.TabIndex = 1;
+            this.txtColumna.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFila_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Columna:";
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(338, 38);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(81, 40);
+            this.btnCrear.TabIndex = 4;
+            this.btnCrear.Text = "Crear matriz";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 255);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 366);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtColumna);
+            this.Controls.Add(this.txtFila);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matriz Botones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFila;
+        private System.Windows.Forms.TextBox txtColumna;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MatrizBotones/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrizBotones
+{
+    public partial class Form1 : Form
+    {
+        Button nuevoBoton = new Button();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              
+                float numero = 0.0f;
+                int num1 = int.Parse(txtFila.Text);
+                int num2 = int.Parse(txtColumna.Text);
+
+               
+
+                Button[,] matrizBoton = new Button[num1, num2];
+                for (int i = 0; i < num1; i++)
+                {
+                   
+                    for (int j = 0; j < num2; j++)
+                    {
+
+                        matrizBoton[i, j] = new Button();
+                        matrizBoton[i, j].Width = 30;
+                        matrizBoton[i, j].Height = 30;
+                        matrizBoton[i, j].Text = Convert.ToString(numero);
+                        matrizBoton[i, j].Top = i * 30;
+                        matrizBoton[i, j].Left = j * 30;
+                        //this.Controls.Add(matrizBoton[i, j]);
+
+                        // nuevoBoton.Text = Convert.ToString(numero);
+                        nuevoBoton = matrizBoton[i, j];
+                        nuevoBoton.Click += new EventHandler(nuevoBoton_Click);
+                        numero += 0.1f;
+                        groupBox1.Controls.Add(nuevoBoton);
+                       
+                    }
+                }
+                txtFila.Text = ""; txtColumna.Text = ""; 
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ingresar valores en los campos", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void nuevoBoton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            ColorDialog color = new ColorDialog();
+            if(color.ShowDialog()== DialogResult.OK)
+            {
+                btn.BackColor = color.Color;
+            }
+        }
+
+        private void txtFila_KeyUp(object sender, KeyEventArgs e)
+        {
+            var txt = sender as TextBox; //Sender obtiene el objeto actual 
+            try
+            {
+                int a = Int32.Parse(txt.Text); //Intentamos covertir el string a número 
+            }
+            catch (Exception )
+            {
+                txt.Text = ""; //En caso de error se limpia el Campo
+            }
+        }
+
+    }
+}
